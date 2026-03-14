531476f791dcb4ed0081dc8d524d09741e68e44c
anika-mestvirishvili-4 <anikamestvirishvili4@gmail.com>
2026-03-15 03:08:11 +0400

    Option to view result statistics added

--- main.py
+++ main.py
@@ -28,17 +28,33 @@ def evaluate_races(races, chosen_skills):
             skill_value = race_skill_value.get(skill, 15)
 
             if skill_value == 25:
-                scores[race] += 3
-            elif skill_value == 20:
                 scores[race] += 2
-            else: 
+            elif skill_value == 20:
                 scores[race] += 1
+            else: 
+                scores[race] += 0
     return scores
 
 def rank_results(chosen_skills):
     scores = evaluate_races(races, chosen_skills)
     sorted_scores = sorted(scores.items(), key=lambda item: item[1], reverse=True)
     return sorted_scores
+
+def statistics(chosen_skills):
+    user_answer = input("Would you like to learn more about the result statistics? (y/n)")
+
+    if  user_answer == "y":
+        
+        for race, score in rank_results(chosen_skills):
+            percent = round((score / 7) * 100) 
+            print(f"{race}: {score}/7 ({percent}%)")
+
+
+    elif user_answer == "n":
+        pass
+    else:
+        print("Input is invalid. (y/n)")
+        statistics(chosen_skills)
 
 def results():
     chosen_skills = user_interference()
@@ -47,5 +63,9 @@ def results():
 
     for race, score in rank_results(chosen_skills)[:3]:
         print(race, f"(Score: {score})")
+    statistics(chosen_skills)
+
+
+
 
 results()
